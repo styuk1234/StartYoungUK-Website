cd5a3184277e99e392ba34aca7b1606864d03520
vidyam99 <vidyajmakwana@gmail.com>
2022-09-25 16:45:24 +0530

    fixed logout page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from sponsor.models import Donation
+from django.core.serializers import serialize
+import json
 
 def home(request):
+    top10_donation = Donation.objects.all().order_by('amount')[:10]
+    serial_donation= json.loads(serialize('json', top10_donation))
+    print(serial_donation)
     return render(request, 'home.html')
