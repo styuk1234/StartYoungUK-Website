15585c30aca321d7a192de1986540317b077179f
vidyam99 <vidyajmakwana@gmail.com>
2022-09-25 13:07:32 +0530

    included receipt

--- sponsor/views.py
+++ sponsor/views.py
@@ -3,7 +3,8 @@ from .forms import DonationForm
 from django.contrib import messages
 from sponsor.models import Donations
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from fpdf import FPDF
 
 def sponsor(request):
     if request.method == 'POST':
@@ -20,8 +21,19 @@ def sponsor(request):
     return render(request, 'sponsor.html', {'form':form})
 
 def sendthankyoumail(email_id):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size = 15)
+    pdf.cell(200, 10, txt = "Acknowlegment Receipt",
+		ln = 1, align = 'C')
+    pdf.cell(200, 10, txt = "Thank you for your donation. Receipt No#1234",
+		ln = 2, align = 'C')
+    pdf.output("Receipt.pdf")
     subject = 'Thank you for your donation!'
-    message = f'Hi, thank you for donation to StartYoungUK charity.'
+    message = f'Hi, thank you for donation to our charity.'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email_id, ]
-    send_mail( subject, message, email_from, recipient_list )
+    email = EmailMessage(
+    subject, message, email_from, recipient_list)
+    email.attach_file('Receipt.pdf')
+    email.send()
