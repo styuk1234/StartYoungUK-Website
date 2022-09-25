6508ff056b0e31389ecf621566351975f6b05e04
vidyam99 <vidyajmakwana@gmail.com>
2022-09-25 12:47:46 +0530

    added email service

--- StartYoungUK/settings.py
+++ StartYoungUK/settings.py
@@ -85,6 +85,13 @@ TEMPLATES = [
 CRISPY_TEMPLATE_PACK = 'uni_form'
 WSGI_APPLICATION = 'StartYoungUK.wsgi.application'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'youngcoders.codefest@gmail.com'
+EMAIL_HOST_PASSWORD = 'dnziblkvhptubmfa'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
--- sponsor/views.py
+++ sponsor/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from .forms import DonationForm
 from django.contrib import messages
 from sponsor.models import Donations
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.mail import send_mail
 
 def sponsor(request):
     if request.method == 'POST':
@@ -10,9 +11,17 @@ def sponsor(request):
         if form.is_valid():
             form.save()
             messages.success(request,f'Thank you for your donation!')
+            sendthankyoumail(form.cleaned_data['email_id'])
         else:
             print('invalid')
             messages.error(request, 'Invalid Form')
 
     form = DonationForm()    
     return render(request, 'sponsor.html', {'form':form})
+
+def sendthankyoumail(email_id):
+    subject = 'Thank you for your donation!'
+    message = f'Hi, thank you for donation to StartYoungUK charity.'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email_id, ]
+    send_mail( subject, message, email_from, recipient_list )
