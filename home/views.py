24d1ecd4c13de292fdb6ce5510b6fdc6ab9e77cd
vidyam99 <vidyajmakwana@gmail.com>
2022-09-27 00:22:11 +0530

    donations pulled from db

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.core.serializers import serialize
 import json
 
 def home(request):
-    top10_donation = Donation.objects.all().order_by('amount')[:10]
-    serial_donation= json.loads(serialize('json', top10_donation))
-    print(serial_donation)
-    return render(request, 'home.html')
+    top_donation = Donation.objects.all().order_by('-amount')[:4]
+    serial_donation= json.loads(serialize('json', top_donation))
+    # print(serial_donation[1]['fields']['name'])
+    return render(request, 'home.html', {'top_donations':serial_donation})
