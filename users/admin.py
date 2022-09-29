5ffcd012d4ccff28488f9ba3535f844cd906e3c4
Yogesh Varal <yogeshvaralhdp@gmail.com>
2022-09-29 10:35:31 +0530

    change admin page title

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,10 @@ from users.models import StartYoungUKUser, Mentor, Child
 
 @admin.register(StartYoungUKUser)
 class StartYoungUKUserAdmin(admin.ModelAdmin):
+    admin.site.site_header = 'StartYoungUK Administration'
+    admin.site.site_title = ' '
+    admin.site.index_title = ' '
+
     list_display = ("email", "display_name", "user_type", "is_verified", "phone_number")
     list_filter = ("is_verified", "user_type", )
     search_fields = ("display_name__startswith", "email__startswith", )
