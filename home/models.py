8f25eb3b424f5a995e2642a42ff6ade00c5790cf
pikabe <35191200+pikabe@users.noreply.github.com>
2023-02-12 17:55:49 +0000

    affiliations banner
    added a section for affiliations banner in home page.
    
    Added a placeholder model for corporate sponsors
    
    Added TODO for discussion about database.

--- home/models.py
+++ home/models.py
@@ -13,3 +13,15 @@ class Campaign(models.Model):
 
     def __str__(self):
         return f"{self.campaign_id, self.campaign_title}"
+
+# TODO: discuss in database meeting how to store coporate sponsors, schools, etc
+class Coporate_sponsors(models.Model):
+
+    coporate_sponsor_id = models.AutoField(primary_key=True)
+    coporate_sponsor_name = models.CharField(max_length=50, null=False)
+    coporate_sponsor_description = models.TextField(max_length=200, null=False)
+    # TODO: update upload_to to get corporate sponsor pics
+    coporate_sponsor_image = models.ImageField(default='default.jpg', upload_to='campaign_pics')
+
+    # def __str__(self):
+    #     return f"{self.coporate_sponsor_id, self.coporate_sponsor_name}"
