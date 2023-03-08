9b48f433b1736cd7340aaaa0d54ca2e9d12d3212
pikabe <35191200+pikabe@users.noreply.github.com>
2023-03-08 22:01:19 +0000

    custom PasswordResetCompleteView
    Co-Authored-By: Nguyễn Nhật Mai <111487792+mion158@users.noreply.github.com>

--- StartYoungUK/urls.py
+++ StartYoungUK/urls.py
@@ -44,6 +44,6 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name='password_reset'),
     path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'users/password_reset_complete.html'), name='password_reset_complete'),
     path('buddy/', home_views.buddysystem, name="buddy-system"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
