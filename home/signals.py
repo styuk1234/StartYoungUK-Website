1f5d27214896163147b65f73126069af48e9568a
mion158 <111487792+mion158@users.noreply.github.com>
2023-04-20 16:48:57 +0100

    added embedded logo to email
    Co-Authored-By: pikabe <35191200+pikabe@users.noreply.github.com>

--- home/signals.py
+++ home/signals.py
@@ -1,4 +1,4 @@
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 # from fpdf import FPDF
 from django.conf import settings
 from django.template.loader import render_to_string
@@ -8,6 +8,9 @@ import os
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.template.loader import get_template
+from email.mime.image import MIMEImage
+
+from functools import lru_cache
 
 
 
@@ -35,21 +38,45 @@ def sendBuddyApprovalEmail(email,status):
 
 def sendLetterReminderEmail(email):
     html_tpl_path = 'email_templates/email_template.html'
-    context_data =  {'name': 'JK'}
-    email_html_template = get_template(html_tpl_path).render(context_data)
     receiver_email = [email, ]
     subject = 'Reminder: write letter to your child'
     # Get the path to the logo image file
     logo_path = os.path.join(settings.STATIC_ROOT, 'images', 'startyounguk-logo.jpg')
+    context_data =  {'image_path': logo_path}
+    email_html_template = get_template(html_tpl_path).render(context_data)
+    # email_msg = EmailMessage(subject, 
+    #                             email_html_template, 
+    #                             settings.EMAIL_HOST_USER,
+    #                             receiver_email,
+    #                             reply_to=[settings.EMAIL_HOST_USER]
+    #                             )
+    
+    email_msg = EmailMultiAlternatives(
+        subject=subject,
+        body=email_html_template,
+        from_email=settings.EMAIL_HOST_USER,
+        to=receiver_email,
+        reply_to=[settings.EMAIL_HOST_USER],
 
-    email_msg = EmailMessage(subject, 
-                                email_html_template, 
-                                settings.EMAIL_HOST_USER,
-                                receiver_email,
-                                reply_to=[settings.EMAIL_HOST_USER]
-                                )
-    with open(logo_path, 'rb') as f:
-        image_data = f.read()
-        email_msg.attach('startyounguk-logo.jpg', image_data, 'image/jpg')
+    )
+    # with open(logo_path, 'rb') as f:
+    #     image_data = f.read()
+    #     email_msg.attach('startyounguk-logo.jpg', image_data, 'image/jpg')
     email_msg.content_subtype = 'html'
     email_msg.send(fail_silently=False)
+
+    email_msg.mixed_subtype = 'related'
+    email_msg.attach_alternative(email_html_template, "text/html")
+    email_msg.attach(logo_data())
+
+    email_msg.send(fail_silently=False)
+
+@lru_cache()
+def logo_data():
+    logo_path = os.path.join(settings.STATIC_ROOT, 'images', 'startyounguk-logo.jpg')
+
+    with open(logo_path, 'rb') as f:
+        logo_data = f.read()
+    logo = MIMEImage(logo_data)
+    logo.add_header('Content-ID', '<logo>')
+    return logo
