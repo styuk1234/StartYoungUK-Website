a329f5187a3b16c475edf7751929d48adcf27859
mion158 <111487792+mion158@users.noreply.github.com>
2023-05-05 11:59:34 +0100

    redundant

--- home/views.py
+++ home/views.py
@@ -134,13 +134,11 @@ def letter_tracker(request):
         # three bottom buttons functions
         checked_buddies = request.POST.getlist("chosen-buddies")
         if "send-email" in request.POST:
-            email_content = EmailContent.objects.get(email_type="Letter")
-
             for buddy_id in checked_buddies:
                 buddy = Buddy.objects.get(id=buddy_id)
                 # only send letter to buddy with false letter received
                 if buddy.letter_received == False:
-                    sendEmail(buddy.user.email, email_content.email_type)
+                    sendEmail(buddy.user.email, "Letter")
             return redirect("letter_tracker")
         # update letter received to true
         elif "letter-received-true" in request.POST:
