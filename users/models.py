353c2ca2149ca4565ebd53ae9a21f15bdb0e440f
pikabe <35191200+pikabe@users.noreply.github.com>
2023-05-14 17:49:34 +0100

    added hobbies and occupation to buddy registration

--- users/models.py
+++ users/models.py
@@ -71,7 +71,7 @@ class Buddy(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # name = models.CharField(max_length=50, null=False)
-    # hobbies = models.CharField(max_length=10,default="0000000000")
+    # hobbies = models.CharField(max_length=255,default="")
     description = models.TextField(null=False, default="No description yet.")
     date_status_modified = models.DateTimeField(auto_now_add=True)
     # occupation = models.CharField(max_length=20, null=False)
