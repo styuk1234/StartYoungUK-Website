da56ca0dc9b4ea93ee9ce00bfb1eea6bd079923e
styuk1234 <136232784+styuk1234@users.noreply.github.com>
2023-06-11 11:51:45 +0000

    Added STATIC_FILES_STORAGE key for WhiteNoise

--- StartYoungUK/settings.py
+++ StartYoungUK/settings.py
@@ -163,6 +163,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 STATIC_FILES_DIR = (BASE_DIR / "static",)
+STATIC_FILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
