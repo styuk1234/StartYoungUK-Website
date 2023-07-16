aec40e05d3a99a4b79b5b26b8ce81d109ea95730
Nick Misiiuk <mykyta.misiiuk@gmail.com>
2023-07-16 11:18:25 +0100

    added azure storage config

--- StartYoungUK/settings.py
+++ StartYoungUK/settings.py
@@ -180,6 +180,8 @@ STATIC_URL = "/static/"
 STATIC_FILES_DIR = (BASE_DIR / "static",)
 STATIC_FILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+DEFAULT_FILE_STORAGE = 'azure_storage.custom_azure.PublicAzureStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
