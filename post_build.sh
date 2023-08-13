b0c77a147b52579bdc8b6d8101c35e3612db92a5
Arghyadeep Das <arghyadeep1899@gmail.com>
2023-08-13 17:12:55 +0530

    Updated post_build.sh

--- post_build.sh
+++ post_build.sh
@@ -1,2 +1,2 @@
-python manage.py makemigrations about contact home sponsor users admin
+python manage.py makemigrations about contact home sponsor users admin ipn otp_totp verify_email
 python manage.py migrate
