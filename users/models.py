202473876f1a9fe446c62a8d55840d78a00f016e
Arghyadeep Das <arghyadeep1899@gmail.com>
2023-08-13 17:06:17 +0530

    Removed unwanted commented codes

--- users/models.py
+++ users/models.py
@@ -22,7 +22,6 @@ class StartYoungUKUser(models.Model):
         max_length=10, choices=[("I", "Individual"), ("C", "Corporate")], null=False
     )
     is_verified = models.BooleanField(default=False)
-    #image = models.ImageField(default="profile_pics/default_dp.jpg", upload_to="profile_pics")
     is_coordinator = models.BooleanField(default=False)
     is_buddy = models.BooleanField(default=False)
     sdp_amount = models.PositiveIntegerField(default=0)
@@ -41,37 +40,14 @@ class StartYoungUKUser(models.Model):
         return f"{self.email}"
 
 
-# class Child(models.Model):
-
-#     class Meta:
-#         verbose_name_plural = "Children"
-
-#     child_id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=50, null=False)
-#     age = models.PositiveIntegerField(null=False)
-#     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Others')], null=False)
-#     class_std = models.CharField(max_length=10, null=False)
-#     school = models.CharField(max_length=50, null=False)
-#     hobbies = models.CharField(max_length=10,default="0000000000")
-#     mentor = models.PositiveIntegerField(null=True)
-#     date = models.DateTimeField(auto_created=True)
-
-#     def __str__(self):
-#         return f"{self.child_id}"
-
-
-# TODO do we want to make this a child model of startyoung uk user?
 class Buddy(models.Model):
     class Meta:
         verbose_name_plural = "Buddies"
 
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # name = models.CharField(max_length=50, null=False)
-    # hobbies = models.CharField(max_length=255,default="")
     description = models.TextField(null=False, default="No description yet.")
     date_status_modified = models.DateTimeField(auto_now_add=True)
-    # occupation = models.CharField(max_length=20, null=False)
     status = models.CharField(
         max_length=256,
         choices=[
